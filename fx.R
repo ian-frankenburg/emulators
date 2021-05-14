3cc3297fc1e4b0f4b3a7af96ee526a0444b9c62d
Ian Frankenburg <31015994+ian-frankenburg@users.noreply.github.com>
2021-05-14 14:40:19 -0700

    fx

--- fx.R
+++ fx.R
@@ -10,16 +10,16 @@ getSymbols("GOOGL", from = '2019-01-01',
            auto.assign = TRUE)
 y1=log(as.numeric(GOOGL$GOOGL.Close))
 y2=y1+rnorm(length(y1),0,.05)
-y=cbind(y1,y2)
-matplot(cbind(y1,y2),type="l")
+y=cbind(y1)
+matplot(y,type="l")
 #y=cbind(y1,y2)
 matplot(y,type="l")
 # int<lower=0> N; // length of time series
 # int q; // number of time series
 # int p; //  lag order if AR dynamic regression matrix
 # vector[q] y[N];
-model_dat = list("y"=matrix(y,ncol=2),"p"=1,q=2,N=nrow(y))
-compiled = cmdstan_model(stan_file="kalman_filter2.stan")
+model_dat = list("y"=matrix(y,ncol=1),"p"=1,q=1,N=length(y))
+compiled = cmdstan_model(stan_file="dlm_kalman.stan")
 sample <- compiled$sample(
   data = model_dat,
   chains = 2,
@@ -28,6 +28,6 @@ sample <- compiled$sample(
   max_treedepth = 10
 )
 
-ypred=matrix(sample$summary("ypred")$mean,ncol=2)
-matplot(ypred,type="l")
-matplot(ypred[,2],type="l",col="darkblue",add=T)
+ypred=matrix(sample$summary("ypred")$mean,ncol=1)
+matplot(ypred,type="l",lwd = 2,lty=1,col="darkblue")
+matplot(y,type="l",col="darkred",add=T)
